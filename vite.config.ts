832e401264c1a406ac9fad26e2ea0d2dd4139dbd
Ivan <ivan@Ivans-MacBook-Air.local>
2023-03-27 19:00:07 +0700

    init

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import postcss from "./postcss.config.cjs";
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // Prevent vite from obscuring rust errors
+  clearScreen: false,
+  server: {
+    strictPort: true,
+  },
+  envPrefix: ["VITE_", "TAURI_"],
+  build: {
+    target: ["es2021", "chrome100", "safari13"],
+    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
+  plugins: [svelte()],
+  css: {
+    postcss,
+  },
+});
